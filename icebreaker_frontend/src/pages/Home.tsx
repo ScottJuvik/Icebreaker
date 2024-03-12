59b7c68d3e0e874dcdc9dace5b5498fe4e417d4b
emsoraffa <emil.rafael.solberg@gmail.com>
2024-03-12 15:20:18 +0100

    feat: add create review

--- icebreaker_frontend/src/pages/Home.tsx
+++ icebreaker_frontend/src/pages/Home.tsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import SearchBar from "../components/SearchBar";
 import Activities from "../components/Activities";
-import ActivityCard from "../components/ActivityCard";
 import { Activity } from "../types/types";
-//import { getActivities } from "../api/ActivitiesAPI";
 import { db } from "../firebase/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 
